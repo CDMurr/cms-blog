896e82d69089e8435f3f015ed801663c9411fff0
Cody Murray <cdillon.murray@yahoo.com>
2022-05-13 20:02:54 -0500

    created model files

---
+++ server.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+// app.use(express.static(path.join(__dirname, 'public')));
+
+const express = require('express');
+// const routes = require('./routes');
+const sequelize = require('./config/connection');
+
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+
+// turn on routes
+// app.use(routes);
+
+// turn on connection to db and server
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening to you'));
+});
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+
+
+
+
